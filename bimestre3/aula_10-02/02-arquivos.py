d1f0c6f812067a7dcc289f85f025779f7712a798
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-10-02 11:56:45 -0300

    adicionada aula 10-02

---
+++ bimestre3/aula_10-02/02-arquivos.py
@@ -0,0 +1,1 @@
+# ler um número e gerar um arquivo com a tabuada desse número
---
+++ bimestre3/resumo.py
@@ -0,0 +1,9 @@
+# arquivos
+var = open("file", "mode")
+var.read(1)  # size
+var.write("text")
+var.readline()
+var.readlines()
+var.close()
+# ou
+# with open(file, mode) as var:
