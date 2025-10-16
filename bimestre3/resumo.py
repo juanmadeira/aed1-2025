7128a7dfd5f294049c00c4f5777aea5afe3a108a
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-10-16 20:09:32 -0300

    atualizado resumo

--- bimestre3/resumo.py
+++ bimestre3/resumo.py
@@ -1,9 +1,13 @@
-# arquivos
-var = open("file", "mode")
-var.read(1)  # size
-var.write("text")
-var.readline()
-var.readlines()
-var.close()
-# ou
-# with open(file, mode) as var:
+#  _ __ ___  ___ _   _ _ __ ___   ___   _ __  _   _
+# | '__/ _ \/ __| | | | '_ ` _ \ / _ \ | '_ \| | | |
+# | | |  __/\__ \ |_| | | | | | | (_) || |_) | |_| |
+# |_|  \___||___/\__,_|_| |_| |_|\___(_) .__/ \__, |
+#                                      |_|    |___/
+
+with open("file", "mode") as file:  # ou file = open("file", "mode")
+    file.read(1)  # size
+    file.readline()
+    file.readlines()
+    file.close()
+    file.seek(0) # reseta o ponteiro
+    file.write("text")
