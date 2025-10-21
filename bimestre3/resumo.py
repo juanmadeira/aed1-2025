23c57c6ffa7a40c870cd2ab66f8604270ad81000
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-10-20 23:33:43 -0300

    atualizado resumo

--- bimestre3/resumo.py
+++ bimestre3/resumo.py
@@ -4,10 +4,12 @@
 # |_|  \___||___/\__,_|_| |_| |_|\___(_) .__/ \__, |
 #                                      |_|    |___/
 
-with open("file", "mode") as file:  # ou file = open("file", "mode")
+# encodings ISO-8859-1 e latin-1 são o mesmo
+with open("file", "mode", encoding="ISO-8859-1") as file:
+    # ou file = open("file", "mode")
     file.read(1)  # size
     file.readline()
     file.readlines()
     file.close()
-    file.seek(0) # reseta o ponteiro
+    file.seek(0)  # reseta o ponteiro
     file.write("text")
