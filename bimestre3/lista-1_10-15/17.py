540603d1e3d8d2f970e1282e1ffda22a259e7769
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-10-21 22:31:43 -0300

    corrigido exercício 17

--- bimestre3/lista-1_10-15/17.py
+++ bimestre3/lista-1_10-15/17.py
@@ -2,28 +2,34 @@
 # e gere um arquivo pareseimpares.txt com todas as linhas dos outros dois arquivos, de forma a preservar a ordem numérica.
 # para ler os dados contidos nos arquivos pares.txt e impares.txt, o programa somente poderá utilizar a função readlines().
 # a medida que um dado é lido de um dos arquivos de entrada, este já deve ser salvo no arquivo pareseimpares.txt, respeitando a ordem numérica dos dados.
-with open("17-pares.txt", "r") as file_even:
-    with open("17-impares.txt", "r") as file_odd:
-        todos = [0, 0]
-        content_even = file_even.readlines()
-        content_odd = file_odd.readlines()
-        for i, j in zip(content_even, content_odd):
-            i = int(i)
-            j = int(j)
-            if i < todos[-2]:
-                todos.insert(-2, i)
-            elif i < todos[-1]:
-                todos.insert(-1, i)
-            else:
-                todos.append(i)
-            if j < todos[-2]:
-                todos.insert(-2, j)
-            elif j < todos[-1]:
-                todos.insert(-1, j)
-            else:
-                todos.append(j)
+with open("17-pares.txt", "r") as even, open("17-impares.txt", "r") as odd:
+    todos = [0, 0]
+    while True:
+        even_line = even.readline()
+        odd_line = odd.readline()
 
-        del todos[0:2]
+        if not even_line and not odd_line:
+            break
+
+        if even_line.strip():
+            even_i = int(even_line.strip())
+            if even_i < todos[-2]:
+                todos.insert(-2, even_i)
+            elif even_i < todos[-1]:
+                todos.insert(-1, even_i)
+            else:
+                todos.append(even_i)
+
+        if odd_line.strip():
+            odd_i = int(odd_line.strip())
+            if odd_i < todos[-2]:
+                todos.insert(-2, odd_i)
+            elif odd_i < todos[-1]:
+                todos.insert(-1, odd_i)
+            else:
+                todos.append(odd_i)
+
+    del todos[0:2]
 
 with open("17.txt", "w") as file:
     for i in todos:
