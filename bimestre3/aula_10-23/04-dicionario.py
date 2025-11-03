370780ac5e3b46c79448f17b33d9d1fc6c3975af
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-11-02 21:37:57 -0300

    movida aula 10-23 para o terceiro bimestre

--- bimestre3/aula_10-23/04-dicionario.py
+++ bimestre3/aula_10-23/04-dicionario.py
@@ -2,16 +2,19 @@
 # faça uma busca a partir de um nome lido e mostre o telefone
 contatos = {
     "Maria": {
-        "telefone": 9999,
+        "telefone": 53981234567,
         "email": "maria@protonmail.com"
     },
     "João": {
-        "telefone": 7777,
+        "telefone": 21982345678,
         "email": "joao@protonmail.com"
     }
 }
 
 def buscaTel(nome):
-    return nome
+    for i in contatos:
+        if i.upper() == nome.upper():
+            return f"O telefone de {i} é {contatos[i]['telefone']}"
 
 nome = input("Insira um nome para buscar seu telefone: ")
+print(buscaTel(nome))
